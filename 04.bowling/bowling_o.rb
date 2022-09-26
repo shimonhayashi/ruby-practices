5954fd14d83fa76c6882682e7543de16d3c21d0e
shimonhayashi <hayashi.shimon@donuts.ne.jp>
2022-09-26 22:43:11 +0900

    指摘事項を修正

--- 04.bowling/bowling_o.rb
+++ 04.bowling/bowling_o.rb
@@ -4,35 +4,33 @@ score = ARGV[0]
 scores = score.split(',')
 
 shots = []
+nine_frame = 18 # 9フレーム目までの最大投球数
+
 scores.each do |s|
-  if shots.size <= 17 && s == 'X' # 9フレーム目までのstrikeの処理
+  if shots.size <= nine_frame - 1 && s == 'X' # 9フレーム目までのstrikeの処理
     shots << 10
     shots << 0
-  elsif shots.size >= 18 && s == 'X' # 10フレーム目のstrikeの処理
+  elsif shots.size >= nine_frame && s == 'X' # 10フレーム目のstrikeの処理
     shots << 10
   else
     shots << s.to_i
   end
 end
 
-frames = []
-shots.each_slice(2) do |s|
-  frames << s
-end
+frames = shots.each_slice(2).to_a # 全投球を1フレーム2投球ずつに分割する
 
-point = 0
-frames.each_with_index do |frame, i| # each_with_indexでframesの順番を得る
+point = frames.each_with_index.sum do |frame, i| # each_with_index.sumでframesの合計と順番を得る
   next_frame = frames[i + 1]
-  point += if frame.size == 1 # 10フレームで3投の場合
-             frame.sum
-           elsif frame[0] == 10 && next_frame[0] == 10 && i <= 7 # 連続strike  8フレームよりも前フレームに適用
-             10 + next_frame.sum + frames[i + 2][0]
-           elsif frame[0] == 10 && i <= 8 # strike 9フレームよりも前フレームに適用
-             10 + next_frame[0] + next_frame[1]
-           elsif frame.sum == 10 && i <= 8 # spare 9フレームよりも前フレームに適用
-             10 + next_frame[0]
-           else
-             frame.sum
-           end
+  if i == 10 # 10フレームの3頭目の処理
+    frame.sum
+  elsif frame[0] == 10 && next_frame[0] == 10 && i <= 7 # 連続strikeの処理  8フレームよりも前フレームに適用
+    10 + next_frame.sum + frames[i + 2][0]
+  elsif frame[0] == 10 && i <= 8 # strikeの処理 9フレームよりも前フレームに適用
+    10 + next_frame[0] + next_frame[1]
+  elsif frame.sum == 10 && i <= 8 # spareの処理 9フレームよりも前フレームに適用
+    10 + next_frame[0]
+  else
+    frame.sum
+  end
 end
 puts point
