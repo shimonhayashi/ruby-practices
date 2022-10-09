1619357b071f1eb0434c8d943413c4309660d4ba
shimonhayashi <hayashi.shimon@donuts.ne.jp>
2022-10-09 14:08:26 +0900

    コメントを受けて2回目の提出

--- 05.ls/ls_command.rb
+++ 05.ls/ls_command.rb
@@ -1,45 +1,32 @@
 # frozen_string_literal: true
 
-# カレントフォルダを引数のフォルダに変更
-Dir.chdir(ARGV[0])
+@path = ARGV[0]
 
-# 該当フォルダに存在するファイルのみ読み込む
-dir = Dir.glob('*')
+files = Dir.glob("#{@path}*")
 
-# 最大カラム数の設定※カラム数は変動できるように変数化
-maximum_column = 3.0
-total_files = dir.size
+MAXIMUM_COLUMN = 3.0
+total_files_number = files.size
 
-# 個数と最大列数で割り縦の数を決める※割り切れない場合もあるから小数点ありで計算し切り上げる
-column = (total_files.to_f / maximum_column).ceil(0)
+column_number = (total_files_number.to_f / MAXIMUM_COLUMN).ceil(0)
 
-# columnの数ごとに配列を作成
-files = []
-dir.each_slice(column) { |n| files << n }
+columns = files.each_slice(column_number).to_a
 
 # 配列ごとの最後に何もなかった場合'nil'を入れる
-(maximum_column - total_files % maximum_column).to_i.times { files[-1] << nil } if files.size >= maximum_column && total_files % maximum_column != 0
-
-# マルチバイトのファイル名があった場合に横幅を揃える。1byteは1byteにしそれ以外は2byteに。横幅を設定し、合間を' 'で埋める。
-class String
-  def mb_ljust(width, padding = ' ')
-    output_width = each_char.map { |c| c.split(//u).length == 1 ? 1 : 2 }.reduce(0, &:+)
-    padding_size = [0, width - output_width].max
-    self + padding * padding_size
+if columns.size >= MAXIMUM_COLUMN && total_files_number % MAXIMUM_COLUMN != 0
+  (MAXIMUM_COLUMN - total_files_number % MAXIMUM_COLUMN).to_i.times do
+    columns[-1] << nil
   end
 end
 
-# 配列の行列をtranspose入れ替え、長いファイル名を横幅にし左揃えに。
-def show_files(dir, files)
-  sorted_files = files.transpose
-  longest_name = dir.max_by(&:size)
-  sorted_files.each do |sorted_file|
-    sorted_file.each do |s|
-      print s.to_s.mb_ljust(longest_name.size, ' ')
+def show_files(files, columns)
+  trans_columns = columns.transpose
+  longest_name = files.max_by(&:size)
+  trans_columns.each do |t|
+    t.each do |s|
+      print s.to_s.gsub(@path, '').ljust(longest_name.size, ' ')
     end
     print "\n"
   end
 end
 
-# 出力する
-show_files(dir, files)
+show_files(files, columns)
