415e129a32db465da20ed41a746e76afcdd1dd0c
shimonhayashi <hayashi.shimon@donuts.ne.jp>
2022-10-10 15:37:26 +0900

    コメントを受けて３回目の提出

--- 05.ls/ls_command.rb
+++ 05.ls/ls_command.rb
@@ -1,32 +1,42 @@
 # frozen_string_literal: true
 
 @path = ARGV[0]
+MAXIMUM_COLUMN = 3
 
-files = Dir.glob("#{@path}*")
+@files = if ARGV.size >= 1
+           if @path.match?(%r{.+/})
+             Dir.glob("#{@path}*")
+           elsif @path.match?(%r{.+[^/]})
+             Dir.glob("#{@path}/*")
+           else
+             Dir.glob('*')
+           end
+         else
+           @path = ''
+           Dir.glob('*')
+         end
 
-MAXIMUM_COLUMN = 3.0
-total_files_number = files.size
-
-column_number = (total_files_number.to_f / MAXIMUM_COLUMN).ceil(0)
-
-columns = files.each_slice(column_number).to_a
-
-# 配列ごとの最後に何もなかった場合'nil'を入れる
-if columns.size >= MAXIMUM_COLUMN && total_files_number % MAXIMUM_COLUMN != 0
-  (MAXIMUM_COLUMN - total_files_number % MAXIMUM_COLUMN).to_i.times do
-    columns[-1] << nil
+def main
+  total_files_count = @files.size
+  column_number = (total_files_count.to_f / MAXIMUM_COLUMN).ceil(0)
+  columns = @files.each_slice(column_number).to_a
+  # 配列ごとの最後に何もなかった場合'nil'を入れる
+  if columns.size >= MAXIMUM_COLUMN && total_files_count % MAXIMUM_COLUMN != 0
+    (MAXIMUM_COLUMN - total_files_count % MAXIMUM_COLUMN).to_i.times do
+      columns[-1] << nil
+    end
   end
+  show_files(@files, columns)
 end
 
 def show_files(files, columns)
-  trans_columns = columns.transpose
   longest_name = files.max_by(&:size)
-  trans_columns.each do |t|
-    t.each do |s|
-      print s.to_s.gsub(@path, '').ljust(longest_name.size, ' ')
+  columns.transpose.each do |column|
+    column.each do |column_show|
+      print column_show.to_s.gsub(%r{#{@path}/*}, '').ljust(longest_name.size, ' ')
     end
     print "\n"
   end
 end
 
-show_files(files, columns)
+main
