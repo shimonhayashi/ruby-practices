d863c3af4f723c41df003ea995109ed5256f5b97
shimonhayashi <hayashi.shimon@donuts.ne.jp>
2022-10-11 18:51:16 +0900

    コメントを受けて4回目の修正

--- 05.ls/ls_command.rb
+++ 05.ls/ls_command.rb
@@ -1,39 +1,32 @@
 # frozen_string_literal: true
 
-@path = ARGV[0]
 MAXIMUM_COLUMN = 3
 
-@files = if ARGV.size >= 1
-           if @path.match?(%r{.+/})
-             Dir.glob("#{@path}*")
-           elsif @path.match?(%r{.+[^/]})
-             Dir.glob("#{@path}/*")
-           else
-             Dir.glob('*')
-           end
-         else
-           @path = ''
-           Dir.glob('*')
-         end
-
 def main
-  total_files_count = @files.size
-  column_number = (total_files_count.to_f / MAXIMUM_COLUMN).ceil(0)
-  columns = @files.each_slice(column_number).to_a
+  path = if ARGV.size >= 1
+           File.join(ARGV[0], '*')
+         else
+           '*'
+         end
+  files = Dir.glob(path)
+  total_files_count = files.size
+  column_count = (total_files_count.to_f / MAXIMUM_COLUMN).ceil
+  columns = files.each_slice(column_count).to_a
+
   # 配列ごとの最後に何もなかった場合'nil'を入れる
   if columns.size >= MAXIMUM_COLUMN && total_files_count % MAXIMUM_COLUMN != 0
     (MAXIMUM_COLUMN - total_files_count % MAXIMUM_COLUMN).to_i.times do
       columns[-1] << nil
     end
   end
-  show_files(@files, columns)
+  show_files(columns)
 end
 
-def show_files(files, columns)
-  longest_name = files.max_by(&:size)
-  columns.transpose.each do |column|
-    column.each do |column_show|
-      print column_show.to_s.gsub(%r{#{@path}/*}, '').ljust(longest_name.size, ' ')
+def show_files(columns)
+  longest_name = columns.flatten.max_by(&:size)
+  columns.transpose.each do |file_tables|
+    file_tables.each do |file_table|
+      print File.basename(file_table.to_s).ljust(longest_name.size)
     end
     print "\n"
   end
