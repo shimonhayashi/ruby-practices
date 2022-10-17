4300a6f66d0d93d4f5fbfaba308fa7cd566bfade
shimonhayashi <hayashi.shimon@donuts.ne.jp>
2022-10-17 13:00:37 +0900

    ls_day2_third

--- 05.ls/ls_command.rb
+++ 05.ls/ls_command.rb
@@ -5,32 +5,26 @@ require 'optparse'
 MAXIMUM_COLUMN = 3
 
 def main
+  files = attribute
+  file_table = slice_column(files)
+  show_files(files, file_table)
+end
+
+def attribute
   params = ARGV.getopts('a')
-  files = if params['a'] && ARGV[0]
-            Dir.entries(ARGV[0]).sort
-          elsif params['a']
-            Dir.entries('.').sort
-          else
-            path = ARGV.size >= 1 ? File.join(ARGV[0], '*') : '*'
-            Dir.glob(path)
-          end
-  count_file(files)
+  opt = OptionParser.new
+  path = opt.parse(ARGV)[0]
+  a_flag = params['a'] ? File::FNM_DOTMATCH : 0
+  Dir.glob('*', a_flag, base: path).sort
 end
 
-def count_file(files)
-  total_files_count = files.size
-  column_count = (total_files_count.to_f / MAXIMUM_COLUMN).ceil
-  file_table = files.each_slice(column_count).to_a
-
-  if file_table.size >= MAXIMUM_COLUMN && total_files_count % MAXIMUM_COLUMN != 0
-    (MAXIMUM_COLUMN - total_files_count % MAXIMUM_COLUMN).times do
-      file_table[-1] << ''
-    end
-  end
-  show_files(file_table)
+def slice_column(files)
+  column_count = (files.size.to_f / MAXIMUM_COLUMN).ceil
+  files.each_slice(column_count).to_a
 end
 
-def show_files(file_table)
+def show_files(files, file_table)
+  (MAXIMUM_COLUMN - files.size % MAXIMUM_COLUMN).times { file_table[-1] << '' } if file_table.size >= MAXIMUM_COLUMN && files.size % MAXIMUM_COLUMN != 0
   column_width = file_table.flatten.max_by(&:size).size
   file_table.transpose.each do |file_paths|
     file_paths.each do |file_path|
