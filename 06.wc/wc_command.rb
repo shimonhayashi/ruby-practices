64d4562dd1994ea5b643eb5f62e3d2772d8235cc
shimonhayashi <hayashi.shimon@donuts.ne.jp>
2022-12-05 12:46:45 +0900

    ３回目の修正。主には引数の改変を修正

--- 06.wc/wc_command.rb
+++ 06.wc/wc_command.rb
@@ -8,12 +8,12 @@ def main
   texts = files.empty? ? [$stdin.read] : files.map { |file| File.read(file) }
   number_lists = word_count(texts, params)
   number_lists_with_space = add_space(number_lists)
-  number_lists_with_filename = add_filename(files, number_lists_with_space)
-  show(files, number_lists_with_filename)
+  number_sum_list = calculate_total(number_lists)
+  show(files, number_lists_with_space, number_sum_list)
 end
 
 def word_count(texts, params)
-  number_lists = texts.map do |str|
+  texts.map do |str|
     numbers = []
     numbers << count_lines(str) << count_words(str) << count_bytes(str) if params.values.none?
     numbers << count_lines(str) if params[:l]
@@ -21,7 +21,6 @@ def word_count(texts, params)
     numbers << count_bytes(str) if params[:c]
     numbers
   end
-  texts.size > 1 ? calculate_total(number_lists) : number_lists
 end
 
 def count_lines(texts)
@@ -37,8 +36,10 @@ def count_bytes(texts)
 end
 
 def calculate_total(number_lists)
-  total_numbers = number_lists.transpose.map(&:sum)
-  number_lists.push(total_numbers)
+  number_sum_list = number_lists.transpose.map(&:sum)
+  number_sum_list.map do |number|
+    number.to_s.rjust(8)
+  end
 end
 
 def add_space(number_lists)
@@ -49,20 +50,14 @@ def add_space(number_lists)
   end
 end
 
-def add_filename(files, number_lists_with_space)
+def show(files, number_lists_with_space, number_sum_list)
   number_lists_with_space.each_with_index do |number_list_with_space, i|
-    number_list_with_space.push(" #{files[i]}")
+    puts "#{number_list_with_space.join} #{files[i]}"
   end
-end
-
-def show(files, number_lists_with_filename)
-  number_lists_with_filename.each_with_index do |number_list_with_filename, i|
-    if files.size > 1 && i == number_lists_with_filename.size - 1
-      puts "#{number_list_with_filename.join} total"
-    else
-      puts number_list_with_filename.join
-    end
+  number_sum_list.each do |number|
+    print number if files.size > 1
   end
+  puts ' total' if files.size > 1
 end
 
 main
